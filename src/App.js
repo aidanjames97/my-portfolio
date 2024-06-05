a1a11a19fc20c30465ffacbe9fd9a9aa0fdb5ce2
Aidan James <ajame23@uwo.ca>
2024-06-05 08:09:40 -0400

    added analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import site from "./site.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route path="/" Component={site}/>
+          <Analytics/>
         </Routes>  
     </Router>
   );
