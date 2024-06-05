186e2ea01a00c1090cc49356e39a36d5fe4c3d6f
Aidan James <ajame23@uwo.ca>
2024-06-05 14:15:10 -0400

    removed analytics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import site from "./site.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route path="/" Component={site}/>
-          <Analytics/>
         </Routes>  
     </Router>
   );
